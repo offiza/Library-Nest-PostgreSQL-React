da790a0b199e8d3b3e420f61c1d7f72eb7ace33d
offiza <matveygorelik070118@gmail.com>
2022-01-03 12:07:24 +0200

    Fixed user roles

--- library/src/roles/user-roles.model.ts
+++ library/src/roles/user-roles.model.ts
@@ -2,7 +2,7 @@ import { Column, DataType, Table, Model, BelongsToMany, ForeignKey } from "seque
 import { User } from "src/users/users.model";
 import { Role } from "./roles.model";
 
-@Table({tableName: 'roles', createdAt: false, updatedAt: false})
+@Table({tableName: 'user_roles', createdAt: false, updatedAt: false})
 export class UserRoles extends Model<UserRoles>{
 
   @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey:true})
--- library/src/users/users.service.ts
+++ library/src/users/users.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { RolesService } from 'src/roles/roles.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 
 @Injectable()
 export class UsersService {
 
-    constructor(@InjectModel(User) private user: typeof User){}
+    constructor(@InjectModel(User) private user: typeof User, private roleService: RolesService){}
 
     async createUser(dto: CreateUserDto){
         const user = await this.user.create(dto);
+        const role = await this.roleService.getRoleByValue("user");
+        await user.$set('roles', [role.id]);
         return user;
     }
 
     async getAllUsers(){
-        const users = await this.user.findAll();
+        const users = await this.user.findAll( {include: {all: true}});
         return users;
     }
 }
